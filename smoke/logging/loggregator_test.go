c84b45874dddd99392271a243718c17dd6564b2e
Ben Moss <bmoss@pivotal.io>
2015-10-05 17:11:34 -0400

    Add Windows smoke tests
    Signed-off-by: Matthew Horan <mhoran@pivotal.io>

--- smoke/logging/loggregator_test.go
+++ smoke/logging/loggregator_test.go
@@ -17,26 +17,46 @@ var _ = Describe("Loggregator:", func() {
 	var appName string
 
 	Describe("cf logs", func() {
-		BeforeEach(func() {
-			appName = testConfig.LoggingApp
-			if !useExistingApp {
-				appName = generator.RandomName()
-				Expect(cf.Cf("push", appName, "-p", SIMPLE_RUBY_APP_BITS_PATH, "-d", testConfig.AppsDomain).Wait(CF_PUSH_TIMEOUT_IN_SECONDS)).To(Exit(0))
-			}
-		})
-
 		AfterEach(func() {
 			if testConfig.Cleanup && !useExistingApp {
 				Expect(cf.Cf("delete", appName, "-f", "-r").Wait(CF_TIMEOUT_IN_SECONDS)).To(Exit(0))
 			}
 		})
 
-		It("can see app messages in the logs", func() {
-			Eventually(func() *Session {
-				appLogsSession := cf.Cf("logs", "--recent", appName)
-				Expect(appLogsSession.Wait(CF_TIMEOUT_IN_SECONDS)).To(Exit(0))
-				return appLogsSession
-			}, CF_TIMEOUT_IN_SECONDS*5).Should(Say(`\[(App|APP)/0\]`))
+		Context("linux", func() {
+			BeforeEach(func() {
+				if !useExistingApp {
+					appName = generator.RandomName()
+					Expect(cf.Cf("push", appName, "-p", SIMPLE_RUBY_APP_BITS_PATH, "-d", testConfig.AppsDomain).Wait(CF_PUSH_TIMEOUT_IN_SECONDS)).To(Exit(0))
+				}
+			})
+
+			It("can see app messages in the logs", func() {
+				Eventually(func() *Session {
+					appLogsSession := cf.Cf("logs", "--recent", appName)
+					Expect(appLogsSession.Wait(CF_TIMEOUT_IN_SECONDS)).To(Exit(0))
+					return appLogsSession
+				}, CF_TIMEOUT_IN_SECONDS*5).Should(Say(`\[(App|APP)/0\]`))
+			})
+		})
+
+		Context("windows", func() {
+			BeforeEach(func() {
+				smoke.SkipIfWindows(testConfig)
+
+				appName = generator.RandomName()
+				Expect(cf.Cf("push", appName, "-p", SIMPLE_DOTNET_APP_BITS_PATH, "-d", testConfig.AppsDomain, "-s", "windows2012R2", "-b", "binary_buildpack", "--no-start").Wait(CF_PUSH_TIMEOUT_IN_SECONDS)).To(Exit(0))
+				smoke.EnableDiego(appName)
+				Expect(cf.Cf("start", appName).Wait(CF_PUSH_TIMEOUT_IN_SECONDS)).To(Exit(0))
+			})
+
+			It("can see app messages in the logs", func() {
+				Eventually(func() *Session {
+					appLogsSession := cf.Cf("logs", "--recent", appName)
+					Expect(appLogsSession.Wait(CF_TIMEOUT_IN_SECONDS)).To(Exit(0))
+					return appLogsSession
+				}, CF_TIMEOUT_IN_SECONDS*5).Should(Say(`\[(App|APP)/0\]`))
+			})
 		})
 	})
 })
