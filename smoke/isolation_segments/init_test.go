86448d03d40b3fd6e075fcf0f9da419a82f73e82
Andrew Chang <anchang@pivotal.io>
2018-12-04 17:15:06 -0800

    Move test setup into init_test.go for iso segs [#161961391]
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- smoke/isolation_segments/init_test.go
+++ smoke/isolation_segments/init_test.go
@@ -2,13 +2,15 @@ package isolation_segments
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers"
 	. "github.com/onsi/ginkgo"
 	ginkgoconfig "github.com/onsi/ginkgo/config"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
-	"os"
-	"path/filepath"
-	"testing"
 
 	"github.com/cloudfoundry/cf-smoke-tests/smoke"
 )
@@ -16,7 +18,19 @@ import (
 func TestSmokeTests(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	testConfig := smoke.GetConfig()
+	testConfig = smoke.GetConfig()
+	testSetup = workflowhelpers.NewSmokeTestSuiteSetup(testConfig)
+
+	SynchronizedBeforeSuite(func() []byte {
+		return nil
+	}, func(data []byte) {
+		testSetup.Setup()
+	})
+
+	SynchronizedAfterSuite(func() {},
+		func() {
+			testSetup.Teardown()
+		})
 	rs := []Reporter{}
 
 	if testConfig.ArtifactsDirectory != "" {
